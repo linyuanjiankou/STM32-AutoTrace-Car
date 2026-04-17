0d68b1c32bb728726a4d7495c4f381a44503689e
LiminalStill <linyuanjiankou@gmail.com>
2026-04-17 18:27:20 +0800

    Add fputc for pritf

---
+++ User/Sys/sys.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "sys.h"
+#include "stm32f1xx_hal.h"
+
+/**
+  * @brief  Retarget printf to UART
+  * @param  ch: Character to send
+  * @param  f: File pointer (unused)
+  * @retval Character sent
+  */
+int fputc(int ch, FILE *f)
+{
+    if (ch == '\n')
+    {
+        HAL_UART_Transmit(&huart1, (uint8_t *)"\r", 1, HAL_MAX_DELAY);
+    }
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    return ch;
+}
---
+++ User/Sys/sys.h
@@ -0,0 +1,14 @@
+#ifndef __SYS_H
+#define __SYS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "usart.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
