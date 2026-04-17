2238803990ab418321f8e8ce61f984192956e236
LiminalStill <linyuanjiankou@gmail.com>
2026-04-18 00:37:06 +0800

    Add abs

--- User/Sys/sys.c
+++ User/Sys/sys.c
@@ -17,3 +17,8 @@ int fputc(int ch, FILE *f)
     HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
     return ch;
 }
+
+uint32_t my_abs(int32_t a){
+    if (a < 0) return -a;
+    else return a;
+}
--- User/Sys/sys.h
+++ User/Sys/sys.h
@@ -5,7 +5,10 @@
 extern "C" {
 #endif
 
+#include <stdio.h>
 #include "usart.h"
+
+uint32_t my_abs(int32_t a);
 
 #ifdef __cplusplus
 }
